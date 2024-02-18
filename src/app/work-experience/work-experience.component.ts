6802e7c71d9ddfebdadc7b391c81ee301f321953
Mohd Sadique <msadique@hawk.iit.edu>
2024-02-17 18:07:48 -0800

    final update

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -6,10 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./work-experience.component.scss']
 })
 export class WorkExperienceComponent {
-  public imoSkills:any = ["Angular", "Python", "PostgreSQL", "HTML5", "CSS3", "Kendo", "Octopus", "AWS"];
-  public amplifiSkills:any = ["Angular", "Java","Spring MVC", "Spring Boot", "MySQL", "HTML5", "CSS3", "Jenkins", "AWS"];
-  public mipSkills:any = ["JavaScript","jQuery","JSP", "Java", "PostgreSQL", "HTML5", "CSS", "Kendo", "Octopus", "AWS"];
-  public amazonSkills:any = ["Angular", "Python", "PostgreSQL", "HTML5", "CSS", "Kendo", "Octopus", "AWS"];
-  public capgeminiSkills:any = ["Angular", "Python", "PostgreSQL", "HTML5", "CSS", "Kendo", "Octopus", "AWS"];
-
+  public googleSkills:any = ["Java", "Python", "Go", "Google Cloud(GCP)","Boq","Kubernetes", "PLX-Script", "Protobuf", "gRPC","pub/sub"];
+  public microsoftSkills:any = ["C#","Python","Azure Cloud","Machine Learning", "PowerShell","PowerBI","Rest APIs","Unix","SQL Server"];
+  public geHealthcareSkills:any = ["Spring MVC", "JAVA (JDK 8)", "Spring Boot","Angular","Maven", "Docker","Kubernetes", "Jira","Rest APIs","Web Services","Spring MVC","MS SQL Server","MS Studio","Unix","Shell Script","HL7 Standard","IntelliJ","Confluence"];
+  public cmeSkills:any = ["Spring MVC", "JAVA (JDK 8)", "Spring Boot",  "Bamboo","Splunk","Rest APIs","Web Services","JSP","Spring MVC","Unix","Shell Script"];
+  public attSkills:any = ["Java" , "ReactJs",  "J2EE", "Angular", "Oracle" , "Pl-Sql", "Rest APIs", "PL-SQL", "Web Services", "JSP", "Windows"];
+  public bibaSkills:any = ["Java", "J2EE", "ReactJS", "TypeScript", "AngularJS", "Spring MVC", "Rest APIs", "CSS", "Git", "PL/ Sql", "MySQL", "Oracle", "XML", "HTML5", "Git", "Apache", , "Apache Tomcat", "Hibernate", "AWS"];
+  public aricentSkills:any = ["C/C++", "Multithreading", "Linux/UNIX", "Network", "Linux Kernel", "Windows", "Shell-Script", "PL/Sql", "Oracle", "MySQL", "GIT"];
 }
